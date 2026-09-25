48c030cf8a06cd3de94698069ccebc1221a84054
RajendraPutraM <andram4heswara@gmail.com>
2026-09-25 13:58:11 +0700

    Kuis1

--- Kuis1/MainSoalA7.java
+++ Kuis1/MainSoalA7.java
@@ -4,21 +4,14 @@ public class MainSoalA7 {
     public static void main(String[] args) {
 
         Kulkas kulkas = new Kulkas("Polytron");
-
         kulkas.tampilkanInfo();
-
-        kulkas.lebihDingin();
-        kulkas.lebihDingin();
+        kulkas.lebihDingin(); kulkas.lebihDingin();
         kulkas.tampilkanInfo();
-
-        kulkas.lebihDingin();
-        kulkas.lebihDingin();
+        kulkas.lebihDingin(); kulkas.lebihDingin();
         kulkas.tampilkanInfo();
-
         kulkas.aturSuhu(5);
-
         kulkas.aturSuhu(9);
-
         kulkas.tampilkanInfo();
+        
     }
 }
